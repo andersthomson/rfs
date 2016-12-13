fa63c018b3a4de1023623b3dd958a0f2b2cb19fc
Anders Thomson <anders.thomson@ericsson.com>
2016-12-13 06:51:20 +0100

    more refactoring

--- rfs.py
+++ rfs.py
@@ -14,16 +14,18 @@ import base64
 import hashlib
 import sys
 
-def hashfile(f):
+def hashfile(fn,start,stop):
     hasher=hashlib.sha512()
-    with open(f, 'rb') as afile:
-        buf = afile.read()
+    with open(fn, 'rb') as f:
+        f.seek(start)
+        buf = f.read(stop-start+1)
         hasher.update(buf)
     return hasher.hexdigest()
 
 class finfo(dict):
-    def __init__(self,fname=None,json_str=None):
+    def __init__(self,fname=None,start=None,stop=None,json_str=None):
         if fname:
+            #Attributes for the whole file
             self['fname']=fname
             os.stat_float_times(False)
             stats=os.stat(fname)
@@ -34,16 +36,26 @@ class finfo(dict):
             self['user']=pwd.getpwuid(stats.st_uid).pw_name
             self['group']=grp.getgrgid(stats.st_gid).gr_name
             self['mode']=stats.st_mode
-            self['sha512']=hashfile(fname)
+            #The following attributes are fragment level
+            if start >= 0:
+                self['sha512']=hashfile(fname,start,stop)
+            self['start']=start
+            self['stop']=stop
+
         elif json_str:
             #print json_str
             f=json.loads(json_str)
             #Lift in known keys
-            for k in ['fname','atime','ctime','mtime','size','user','group','mode', 'sha512']:
-                if f[k]:
+            for k in ['fname','atime','ctime','mtime','size','user','group','mode', 'sha512','start','stop','frag_alloc']:
+                if k in f:
                     self[k]=f[k]
                 else:
                     self[k]=''
+            #Bckwards compatibility stuff
+            if self['start']=='':
+                #Not a fragment aware file 
+                self['start']=0
+                self['stop']=self.get_size()-1
 
     def to_json(self):
         return json.dumps(self,sort_keys=True,indent=4, separators=(',', ': '))
@@ -60,7 +72,7 @@ class finfo(dict):
         return self['group']
 
     def get_size(self):
-        return self['size']
+        return int(self['size'])
 
     def get_atime(self):
         return time.strftime('%x %X',time.localtime(self['atime']))
@@ -77,6 +89,19 @@ class finfo(dict):
     def get_sha512(self):
         return self['sha512']
 
+    def get_start(self):
+        return int(self['start'])
+
+    def get_stop(self):
+        return int(self['stop'])
+
+    def is_fragment(self):
+        return (self.get_stop()-self.get_start()+1< self.get_size())
+
+    def set_fragment_allocations(self,a):
+        self['frag_alloc']=a
+
+
 def load_config():
     config = ConfigParser.ConfigParser()
     config.read([ os.path.expanduser('~/.rfs.conf'), 'rfs.conf'])
@@ -89,7 +114,8 @@ class imap4(imaplib.IMAP4_SSL):
     def __init__(self, host):
         imaplib.IMAP4_SSL.__init__(self,host)
         if args.debug:
-            M.debug=5
+            print 'setting debug'
+            self.debug=5
 
 
     def do_select(self, mailbox):
@@ -103,7 +129,45 @@ class imap4(imaplib.IMAP4_SSL):
                 sys.exit(1)
         return typ, num
 
+    def do_append(self,mailbox, flags, date_time, message):
+        imaplib.IMAP4_SSL.append(self, mailbox, flags, date_time, message)
+        return self.untagged_responses['APPENDUID'][0].split()[1]
 
+class store_c(dict):
+    def __init__(self,conf,section):
+        self['host']=conf.get(section,'host')
+        self['user']=conf.get(section,'user')
+        self['password']=conf.get(section,'password')
+        self['folder']=conf.get(section,'mailbox')
+        self['fragment_size']=conf.get(section,'fragment_size')
+        self.connected=False
+
+    def connect(self):
+        self.connection = imap4(self['host'])
+        self.connection.login(self['user'],self['password'])
+        self.connected=True
+
+    def disconnect(self):
+        try:
+            self.connection.close()
+        except:
+            pass
+
+        try:
+            self.connection.logout()
+        except:
+            pass
+
+    def df(self):
+        if not self.connected:
+            self.connect()
+        q_str=self.connection.getquotaroot('INBOX')
+        q_used=1024*int(q_str[1][1][0].split()[2])
+        q_total=1024*int(q_str[1][1][0].split()[3].split(')')[0])
+
+        q_avail=q_total-q_used
+        return [ q_total, q_used, q_avail ]
+        
 def imap_connect(host,user,password,mailbox):
     M = imap4(host)
     M.login(user,password)
@@ -158,8 +222,10 @@ def cmd_list(args):
                 t_str=t_str+':%s' % uid
                 t_str=t_str+'\t%s' % finf.get_mode()
                 t_str=t_str+' %s' % finf.get_user()
-                t_str=t_str+' %s' % finf.get_group()
-                t_str=t_str+'\t%s' % finf.get_size()
+                t_str=t_str+' %s\t' % finf.get_group()
+                t_str=t_str+'{}'.format(finf.get_stop()-finf.get_start()+1)
+                if finf.is_fragment():
+                    t_str=t_str+'({}..{})'.format(finf.get_start(),finf.get_stop())
                 #t_str=t_str+'\t%s' % finf.get_atime()
                 #t_str=t_str+'%s' % finf.get_ctime()
                 t_str=t_str+'\t%s' % finf.get_mtime()
@@ -171,6 +237,66 @@ def cmd_list(args):
         M.close()
         M.logout()
 
+def put_fragment(config,store,fname, start, stop):
+    msg = email.mime.multipart.MIMEMultipart()
+    msg['Subject']='%s (%s-%s)' %(fname,start,stop)
+
+    #Prepare metadata
+    info=finfo(fname=args.fname,start=start,stop=stop)
+    part=email.mime.application.MIMEApplication(info.to_json(),_encoder=email.encoders.encode_base64)
+    msg.attach(part)
+
+    with open(fname, 'rb') as f:
+        f.seek(start)
+        buf = f.read(stop-start+1)
+        part=email.mime.application.MIMEApplication(buf)
+    msg.attach(part)
+
+    #Start network interactions.
+    M = imap_connect(config.get(store, 'host'),config.get(store, 'user'),config.get(store, 'password'),config.get(store, 'mailbox'))
+    new_uid=M.do_append(config.get(store, 'mailbox'), '', '', msg.as_string())
+    M.close()
+    M.logout()
+    return new_uid
+
+def allocate(config, size):
+    #return a list of allocations for a file
+    # [ [ store, startpos in fname, stoppos in fname ], ...]
+    #While honoring:
+    #   per-store fragment_size as set in config
+    #and  optimizing for:
+    #   TBD
+
+
+    avail={}
+    #Get the current free space on the stores
+    for store in config.get('rfs','stores').split():
+
+        s=store_c(config,store)
+        q_tot, q_used, q_avail = s.df()
+        s.disconnect()
+        avail[store] = q_avail // 1024
+       
+    remain=size
+    pos=0
+    
+    allocations=[]
+    while remain>0:
+        #iterate over the stores
+        for s in config.get('rfs','stores').split():
+            chunk=min(1024*avail[s],1024*int(config.get(s,'fragment_size')),remain)
+            #so, we can allocate chunk kbytes and it is at most what we need
+            allocation=[]
+            allocation = [ s, pos, pos+chunk-1]
+            allocations.append(allocation)
+            #Update file offsets
+            remain -= chunk
+            pos += chunk
+            #Update availability
+            avail[s] -= chunk
+            if remain==0:
+                return allocations
+
 def cmd_put(args):
     config = load_config()
 
@@ -180,25 +306,33 @@ def cmd_put(args):
     else:
         #FIXME: This should be dynamic based on free space etc
         store = config.get('rfs','stores').split()[1]
+    a=allocate(config, os.stat(args.fname).st_size)
+    for chunk in a:
+        new_uid=put_fragment(config,chunk[0],args.fname,chunk[1],chunk[2])
+        chunk.append(new_uid)
+    #Ok, now we have uploaded all fragments, make a final new message with a file ToC.
+    info=finfo(fname=args.fname,start=-1,stop=-1)
+    info.set_fragment_allocations(a)
+    part=email.mime.application.MIMEApplication(info.to_json(),_encoder=email.encoders.encode_base64)
 
     msg = email.mime.multipart.MIMEMultipart()
-    msg['Subject']='%s' % args.fname
+    msg['Subject']='%s (toc)' %(args.fname)
 
-    #Prepare metadata
-    info=finfo(fname=args.fname)
-    part=email.mime.application.MIMEApplication(info.to_json(),_encoder=email.encoders.encode_base64)
     msg.attach(part)
 
-    f=open(args.fname, 'rb')
-    part=email.mime.application.MIMEApplication(f.read())
-    #part['Content-Disposition'] = 'attachment'
-    msg.attach(part)
+    #FIXME: assume it fits in 100KB
+    toc_a=allocate(config,100*1024)
+    #print toc_a
+    #print len(toc_a)
+    if len(toc_a)>1:
+        #We got an allocation split over >1 store, abort.
+        print 'Allocation of TOC cannot straddle multiple stores'
+        sys.exit(1)
 
-    #Start network interactions.
-    M = imap_connect(config.get(store, 'host'),config.get(store, 'user'),config.get(store, 'password'),config.get(store, 'mailbox'))
-    M.append(config.get(store, 'mailbox'), '', '', msg.as_string())
-    M.close()
-    M.logout()
+    #M = imap_connect(config.get(toc_a[0][0], 'host'),config.get(toc_a[0][0], 'user'),config.get(toc_a[0][0], 'password'),config.get(toc_a[0][0], 'mailbox'))
+    #new_uid=M.do_append(config.get(toc_a[0][0], 'mailbox'), '', '', msg.as_string())
+    #M.close()
+    #M.logout()
 
 def cmd_get(args):
     config = load_config()
@@ -219,7 +353,6 @@ def cmd_get(args):
     #loop the payload to find the relevant parts
     for payload in msg.get_payload():
         try:
-            #meta=json.loads(payload.get_payload(decode=True))
             finf = finfo(json_str=payload.get_payload(decode=True))
         except ValueError:
             #Save the ref
@@ -235,7 +368,8 @@ def cmd_get(args):
     f = open(fname,'wb')
     f.write(pl.get_payload(decode=True))
     f.close()
-    fhash=hashfile(fname)
+
+    fhash=hashfile(fname,0,os.stat(fname).st_size-1)
     if fhash != finf.get_sha512():
         print 'ERROR: Retrieved file does not match expected sha512'
     M.close()
@@ -246,12 +380,11 @@ def cmd_df(args):
 
     print 'Remote\t\t\t\t1K-blocks\tUsed\tAvailable\tStore'
     for store in config.get('rfs','stores').split():
-        M = imap_connect(config.get(store, 'host'),config.get(store, 'user'),config.get(store, 'password'),config.get(store, 'mailbox'))
-        q_str=M.getquotaroot('INBOX')
-        q_used=q_str[1][1][0].split()[2]
-        q_total=q_str[1][1][0].split()[3].split(')')[0]
-        q_avail=int(q_total)-int(q_used)
-        print '%s@%s/%s\t%s\t%s\t%s\t%s' %(config.get(store, 'user'),config.get(store, 'host'),config.get(store, 'mailbox'),q_total,q_used,q_avail,store)
+        s=store_c(config,store)
+        s.connect()
+        tot, used, free = s.df()
+        print '%s@%s/%s\t%s\t%s\t%s\t%s' % (s['user'],s['host'],s['folder'],tot/1024,used/1024,free/1024,store)
+
 
 def cmd_rm(args):
     config = load_config()
@@ -260,7 +393,7 @@ def cmd_rm(args):
     M = imap_connect(config.get(store, 'host'),config.get(store, 'user'),config.get(store, 'password'),config.get(store, 'mailbox'))
 
     #Move to Trash, then flag as deleted, then expunge it
-    M.list()
+    #M.list()
     #FIXME this is gmail specific
     res = M.uid('copy', uid, '[Gmail]/Trash')
     new_uid=M.untagged_responses['COPYUID'][0].split()[2]
@@ -296,6 +429,12 @@ def cmd_dump(args):
     M.close()
     M.logout()
  
+config = load_config()
+my_confd_stores = config.get('rfs','stores').split()
+my_stores={}
+for s in my_confd_stores:
+    my_stores[s]=store_c(config, s)
+
 parser = argparse.ArgumentParser(description='RFS, remote file store.')
 parser.add_argument('-d',action='store_true',help='-d debug',dest='debug')
 subparsers = parser.add_subparsers()
